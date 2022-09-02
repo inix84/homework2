dacb9f971a0305c2aafe39aa7c26d0399e4fd639
inix84 <inix84@mail.ru>
2022-09-02 17:52:41 +0500

    done task 4 and 5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("ЗАДАНИЕ 1");
+
         var dog = 8.0;
         System.out.println("значение dog с ЗАДАЧИ 1 ---> " + dog);
         dog = dog + 4;
@@ -23,6 +25,27 @@ public class Main {
         System.out.println("значение paper с ЗАДАЧИ 2 ---> " + paper);
         paper = paper - 7639;
         System.out.println("значение paper с ЗАДАЧИ 3 ---> " + paper);
+        System.out.println();
+
+        System.out.println("ЗАДАНИЕ 2, задача 4");
+        var friend = 19;
+        System.out.println("значение friend начальное ---> " + friend);
+        friend = friend + 2;
+        System.out.println("значение friend увеличенное на 2 ---> " + friend);
+        friend = friend /7;
+        System.out.println("значение friend деленное на 7 ---> " + friend);
+        System.out.println();
+
+        System.out.println("ЗАДАНИЕ 2, задача 5");
+        var frog = 3.5;
+        System.out.println("значение frog начальное ---> " + frog);
+        frog = frog *10;
+        System.out.println("значение frog *10 ---> " + frog);
+        frog = frog /3.5;
+        System.out.println("значение frog деленное на 3.5 ---> " + frog);
+        frog = frog +4;
+        System.out.println("значение frog +4 ---> " + frog);
+        System.out.println();
 
     }
 }
