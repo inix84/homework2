6fd4bc0a19a683e3b5a14c516672a4bc64416257
inix84 <inix84@mail.ru>
2022-09-02 17:12:16 +0500

    done task 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args)
     {
         var dog = 8;
-        System.out.println(dog);
+        System.out.println("значение dog с ЗАДАЧИ 1 ---> " + dog);
+        dog = dog + 4;
+        System.out.println("значение dog с ЗАДАЧИ 2 ---> " + dog);
+
         var cat = 3.6;
-        System.out.println(cat);
+        System.out.println("значение cat с ЗАДАЧИ 1 ---> " + cat);
+        cat = cat + 4;
+        System.out.println("значение cat с ЗАДАЧИ 2 ---> " + cat);
+
         var paper = 763789;
-        System.out.println(paper);
+        System.out.println("значение paper с ЗАДАЧИ 1 ---> " + paper);
+        paper = paper + 4;
+        System.out.println("значение paper с ЗАДАЧИ 2 ---> " + paper);
 
     }
 }
